bb2170a5ebf31f4981fa5d3f57a0156fdc4b38e4
Eduardo Lessa <duduuulessa@gmail.com>
2020-11-04 02:32:15 -0300

    Feat: Next auth added and base credentials provider sign up created. Jwt validations not created yet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
+import { Provider } from 'next-auth/client';
 import { AppProps } from 'next/dist/next-server/lib/router/router';
 import '../styles/index.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  console.log(pageProps);
+  return (
+    <Provider session={pageProps.session}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
