91778a4f85ab121a800c458687ecac4e94183c3c
Eduardo Lessa <duduuulessa@gmail.com>
2020-11-07 23:07:46 -0300

    Feat: Sign up and auto login after sign up

--- pages/api/user.tsx
+++ pages/api/user.tsx
@@ -1,41 +1,40 @@
 import { hash } from 'bcrypt';
-import { NextApiRequest, NextApiResponse } from 'next';
 import { User } from '../../model/User';
 import connect from '../../utils/database';
+import withSession from '../../lib/session';
 
-export default async (
-  req: NextApiRequest,
-  res: NextApiResponse
-): Promise<void> => {
-  if (req.method === 'POST') {
-    await connect();
+export default withSession(
+  async (req, res): Promise<void> => {
+    if (req.method === 'POST') {
+      await connect();
 
-    const {
-      password,
-      email,
-      username,
-      first_name,
-      last_name,
-      birth_date,
-    } = req.body;
-    const hashPass = await hash(password, 10);
-    const user = await User.create({
-      email,
-      username,
-      password: hashPass,
-      first_name,
-      last_name,
-      birth_date,
-    });
-    if (user) {
-      return res.status(200).json({ message: user });
-    } else {
-      return res.status(401).json({ error: 'E-mail already in use' });
+      const { password, email, first_name, last_name, birth_date } = req.body;
+      const hashPass = await hash(password, 10);
+      const user = await User.create({
+        email,
+        password: hashPass,
+        first_name,
+        last_name,
+        birth_date,
+      });
+      if (user) {
+        const obj = {
+          email: user.email,
+          id: user._id,
+        };
+
+        req.session.set('user', obj);
+        await req.session.save();
+
+        return res.status(200).json({ obj });
+      } else {
+        return res.status(401).json({ error: 'E-mail already in use' });
+      }
+    } else if (req.method === 'GET') {
+      await connect();
+
+      const users = await User.find();
+      return res.status(200).json({ users });
     }
-  } else if (req.method === 'GET') {
-    await connect();
-
-    const users = await User.find();
-    return res.status(200).json({ users });
   }
-};
+);
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -3,26 +3,35 @@ import { NextPage } from 'next';
 import { useRef, useState } from 'react';
 import { useRouter } from 'next/router';
 import { useUser } from '../lib/useUser';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const LoginPage: NextPage = () => {
   const emailInput = useRef(null);
   const passwordInput = useRef(null);
+  const firstNameInput = useRef(null);
+  const lastNameInput = useRef(null);
   const { mutateUser } = useUser();
   const Router = useRouter();
+  const [startDate, setStartDate] = useState(new Date());
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const email = emailInput.current.value;
     const password = passwordInput.current.value;
+    const firstName = firstNameInput.current.value;
+    const lastName = lastNameInput.current.value;
+
+    console.log(startDate);
 
     try {
-      const response = await axios.post(
-        'http://localhost:3000/api/auth/login',
-        {
-          email,
-          password,
-        }
-      );
+      const response = await axios.post('http://localhost:3000/api/user', {
+        email,
+        password,
+        first_name: firstName,
+        last_name: lastName,
+        startDate,
+      });
       await mutateUser(response.data);
       Router.push('/');
     } catch (error) {
@@ -31,8 +40,14 @@ const LoginPage: NextPage = () => {
   };
 
   return (
-    <div className="w-full max-w-xs">
-      <form className="w-full max-w-lg">
+    <div className="w-full max-w-xs mx-auto my-16">
+      <h1 className="block uppercase tracking-wide text-gray-800 text-2x1 font-bold mb-4 text-center">
+        Register for free below! 😃
+      </h1>
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        onSubmit={handleSubmit}
+      >
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full px-3">
             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
@@ -43,6 +58,7 @@ const LoginPage: NextPage = () => {
               id="grid-email"
               type="email"
               placeholder="email@example.com"
+              ref={emailInput}
             />
           </div>
         </div>
@@ -53,14 +69,12 @@ const LoginPage: NextPage = () => {
               First Name
             </label>
             <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
               id="grid-first-name"
               type="text"
               placeholder="Jane"
+              ref={firstNameInput}
             />
-            <p className="text-red-500 text-xs italic">
-              Please fill out this field.
-            </p>
           </div>
           <div className="w-full md:w-1/2 px-3">
             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
@@ -71,6 +85,7 @@ const LoginPage: NextPage = () => {
               id="grid-last-name"
               type="text"
               placeholder="Doe"
+              ref={lastNameInput}
             />
           </div>
         </div>
@@ -85,11 +100,34 @@ const LoginPage: NextPage = () => {
               id="grid-password"
               type="password"
               placeholder="******************"
+              ref={passwordInput}
             />
             <p className="text-gray-600 text-xs italic">
               Make it as long and as crazy as you'd like
             </p>
           </div>
+        </div>
+
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+              Birth Date
+            </label>
+            <DatePicker
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              selected={startDate}
+              onChange={(date) => setStartDate(date)}
+              dateFormat="dd/MM/yyyy"
+            />
+          </div>
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Sign Up
+          </button>
         </div>
       </form>
     </div>
