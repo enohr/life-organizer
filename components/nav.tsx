1ce9dfe191f4a668b9d9494cd59c36ec80339f49
Eduardo Lessa <duduuulessa@gmail.com>
2020-11-20 23:37:31 -0300

    Fix: Nav showing text based on user logged in or not logged in

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,10 +1,20 @@
 import axios from 'axios';
 import { NextPage } from 'next';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useCallback } from 'react';
 import { useUser } from '../context/user';
 
 const Nav: NextPage = () => {
+  const router = useRouter();
   const { user } = useUser();
+
+  const logout = useCallback(() => {
+    axios.post('http://localhost:3000/api/auth/logout').then(() => {
+      router.push('/');
+    });
+  }, []);
+
   return (
     <nav className="px-4 shadow-xl fixed top-0 w-full overflow-hidden bg-white z-10">
       <ul className="flex justify-between items-center p-2 mx-8">
@@ -16,24 +26,28 @@ const Nav: NextPage = () => {
           </Link>
         </li>
         <ul className="flex justify-between items-center space-x-4">
-          <Link href="/login">
-            <a className="no-underline font-bold tracking-wider">Sign in</a>
-          </Link>
-          <Link href="/signup">
-            <a className="btn-blue no-underline">Sign up</a>
-          </Link>
-          {/* 
-          
-          <li>
-            <button
-              onClick={() =>
-                axios.post('http://localhost:3000/api/auth/logout')
-              }
-            >
-              Logout
-            </button>
-          </li>
-          */}
+          {user?.isLoggedIn ? (
+            <>
+              <li>
+                <button onClick={logout}>Logout</button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link href="/login">
+                  <a className="no-underline font-bold tracking-wider">
+                    Sign in
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/signup">
+                  <a className="btn-blue no-underline">Sign up</a>
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </ul>
     </nav>
